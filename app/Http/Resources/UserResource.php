a0d11bf770104b2708751ccc7eedf61d25e648d8
serkanursavas <serkan.ursavas@icloud.com>
2023-08-03 15:24:09 +0300

    listing users data

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,10 +15,10 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this.id,
-            'name' => $this.name,
-            'email' => $this.email,
-            'created_at' => $this.created_at->format('Y-m-d H:m:s'),
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
         ];
     }
 }
--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axiosClient from "../axios-client";
+import { Link } from "react-router-dom";
 
 function Users() {
     const [users, setUsers] = useState([]);
@@ -15,14 +16,52 @@ function Users() {
             .get("/users")
             .then(({ data }) => {
                 setLoading(false);
-                setUsers(data);
+                setUsers(data.data);
             })
             .catch(() => {
                 setLoading(false);
             });
     };
 
-    return <div>Users</div>;
+    return (
+        <div>
+            <div className="flex items-center justify-between">
+                <h1>Users</h1>
+                <Link to="/users/new" className="btn-add">
+                    Add new
+                </Link>
+            </div>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((u) => (
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.email}</td>
+                                <td>{u.created_at}</td>
+                                <td>
+                                    <Link to={`/users/${u.id}`}>Edit</Link>
+                                    <button className="btn-delete">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default Users;
